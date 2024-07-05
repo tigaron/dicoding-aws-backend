01030a4f551c675ef44c385c6ef695fc5741ed3c
tigaron <suputra@mail.com>
2024-07-05 16:27:01 +0700

    refactor: use hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+ 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
+    port: 3000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
@@ -11,11 +13,16 @@ const init = async () => {
       },
     },
   });
-
-  server.route(routes);
-
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+ 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
