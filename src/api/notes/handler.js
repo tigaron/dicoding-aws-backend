93ac07e810930f4e3b980e19f334f52585b9f269
tigaron <suputra@mail.com>
2024-07-05 16:42:47 +0700

    feat: add notes validator

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,7 @@
 class NotesHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
 
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -11,6 +12,7 @@ class NotesHandler {
 
   postNoteHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
 
       const noteId = this._service.addNote({
@@ -70,8 +72,9 @@ class NotesHandler {
     }
   }
 
-  putNoteByIdHandler(request) {
+  putNoteByIdHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
   
       this._service.editNoteById(id, request.payload);
@@ -90,7 +93,7 @@ class NotesHandler {
     }
   }
 
-  deleteNoteByIdHandler(request) {
+  deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
 
